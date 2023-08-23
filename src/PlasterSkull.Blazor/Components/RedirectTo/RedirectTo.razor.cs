bada506a2411e2749f7f93fc81d3bf6eba8e7226
Ivan Stuk <ivan.stuk@inbox.ru>
2023-08-23 13:14:53 +0300

    add RedirectTo component

---
+++ src/PlasterSkull.Blazor/Components/RedirectTo/RedirectTo.razor.cs
@@ -0,0 +1,19 @@
+﻿namespace PlasterSkull.Blazor;
+
+public class RedirectTo : ComponentBase
+{
+    #region Params
+
+    [Parameter] public string Uri { get; set; } = "/";
+
+    #endregion
+
+    #region Injects
+
+    [Inject] NavigationManager _navigationManager { get; init; } = null!;
+
+    #endregion
+
+    protected override void OnInitialized()
+        => _navigationManager.NavigateTo(Uri);
+}
